c3f5da7c1eef74e16b269393757dc574c9b2ea29
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-06-03 14:57:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dotenv = require('dotenv');
+require('dotenv').config();
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require("path");
@@ -7,7 +7,6 @@ const path = require("path");
 const connectDB = require('./server/database/connection');
 
 const app = express();
-dotenv.config({ path: 'config.env' })
 const PORT = process.env.PORT || 3000
 
 //log requests
